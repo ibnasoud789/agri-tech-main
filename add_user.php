046411e0bfbe418f94a7c1e9c0a3b42b0d4d7d3b
erinnnnnnnnnnnn <151948665+erinnnnnnnnnnnn@users.noreply.github.com>
2024-04-06 23:47:09 +0600

    add user

---
+++ add_user.php
@@ -0,0 +1,62 @@
+<?php
+
+include 'admin.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userID = $_POST['userID'];
+    $Area = $_POST['Area'];
+    $city = $_POST['city'];
+    $postcode = $_POST['postcode'];
+    $contact_number = $_POST['contact_number'];
+    $user_type = $_POST['user_type'];
+
+    $sql = "INSERT INTO user (userID, Area, city, postcode, contact_number, user_type) VALUES ('$userID', '$Area', '$city', '$postcode','$contact_number','$user_type')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New user added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add User</title>
+    <style>
+        *{background-color:rgb(184, 247, 184);}
+    </style>
+</head>
+<body>
+    <h2>Add User</h2>
+    <form action="add_user.php" method="post">
+        <label for="userID">userID:</label>
+        <input type="text" id="userID" name="userID" required><br><br>
+        
+        <label for="Area">Area:</label>
+        <input type="text" id="Area" name="Area" required><br><br>
+        
+        <label for="city">city:</label>
+        <input type="text" id="city" name="city" required><br><br>
+
+        <label for="postcode">postcode:</label>
+        <input type="text" id="postcode" name="postcode" required><br><br>
+
+        <label for="contact_number">contact_number:</label>
+        <input type="text" id="contact_number" name="contact_number" required><br><br>
+
+        <label for="user_type">user_type:</label>
+        <input type="text" id="user_type" name="user_type" required><br><br>
+        
+        <input type="submit" value="Add User">
+    </form>
+</body>
+</html>
+
+
--- admin_dashboard.php
+++ admin_dashboard.php
@@ -13,6 +13,7 @@ if ($result->num_rows > 0) {
 
 echo("User Details"); 
 
+
 $html_table = '<table border="1">';
 $html_table .= '<tr>';
 foreach (array_keys($data[0]) as $column) {
@@ -30,238 +31,36 @@ $html_table .= '</table>';
 
 echo $html_table;
 
-$conn->close();
-
 ?>
 
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Admin Interface</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<style>
-body {
-    color: #404E67;
-    background: #F5F7FA;
-    font-family: 'Open Sans', sans-serif;
-}
-.table-wrapper {
-    width: 700px;
-    margin: 30px auto;
-    background: #fff;
-    padding: 20px;	
-    box-shadow: 0 1px 1px rgba(0,0,0,.05);
-}
-.table-title {
-    padding-bottom: 10px;
-    margin: 0 0 10px;
-}
-.table-title h2 {
-    margin: 6px 0 0;
-    font-size: 22px;
-}
-.table-title .add-new {
-    float: right;
-    height: 30px;
-    font-weight: bold;
-    font-size: 12px;
-    text-shadow: none;
-    min-width: 100px;
-    border-radius: 50px;
-    line-height: 13px;
-}
-.table-title .add-new i {
-    margin-right: 4px;
-}
-table.table {
-    table-layout: fixed;
-}
-table.table tr th, table.table tr td {
-    border-color: #e9e9e9;
-}
-table.table th i {
-    font-size: 13px;
-    margin: 0 5px;
-    cursor: pointer;
-}
-table.table th:last-child {
-    width: 100px;
-}
-table.table td a {
-    cursor: pointer;
+    <style>
+        *{background-color:rgb(184, 247, 184);}
+        button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
     display: inline-block;
-    margin: 0 5px;
-    min-width: 24px;
-}    
-table.table td a.add {
-    color: #27C46B;
-}
-table.table td a.edit {
-    color: #FFC107;
-}
-table.table td a.delete {
-    color: #E34724;
-}
-table.table td i {
-    font-size: 19px;
-}
-table.table td a.add i {
-    font-size: 24px;
-    margin-right: -1px;
-    position: relative;
-    top: 3px;
-}    
-table.table .form-control {
-    height: 32px;
-    line-height: 32px;
-    box-shadow: none;
-    border-radius: 2px;
-}
-table.table .form-control.error {
-    border-color: #f50000;
-}
-table.table td .add {
-    display: none;
-}
-</style>
-<script>
-$(document).ready(function(){
-	$('[data-toggle="tooltip"]').tooltip();
-	var actions = $("table td:last-child").html();
-	// Append table with add row form on add new button click
-    $(".add-new").click(function(){
-		$(this).attr("disabled", "disabled");
-		var index = $("table tbody tr:last-child").index();
-        var row = '<tr>' +
-            '<td><input type="text" class="form-control" name="User ID" id="User ID"></td>' +
-            '<td><input type="text" class="form-control" name="Area" id="Area"></td>' +
-            '<td><input type="text" class="form-control" name="City" id="City"></td>' +
-            '<td><input type="text" class="form-control" name="Postcode" id="Postcode"></td>' +
-            '<td><input type="text" class="form-control" name="Contact Number" id="Contact Number"></td>' +
-            '<td><input type="text" class="form-control" name="User-type" id="User-type"></td>' +
-			'<td>' + actions + '</td>' +
-        '</tr>';
-    	$("table").append(row);		
-		$("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-	// Add row on add button click
-	$(document).on("click", ".add", function(){
-		var empty = false;
-		var input = $(this).parents("tr").find('input[type="text"]');
-        input.each(function(){
-			if(!$(this).val()){
-				$(this).addClass("error");
-				empty = true;
-			} else{
-                $(this).removeClass("error");
-            }
-		});
-		$(this).parents("tr").find(".error").first().focus();
-		if(!empty){
-			input.each(function(){
-				$(this).parent("td").html($(this).val());
-			});			
-			$(this).parents("tr").find(".add, .edit").toggle();
-			$(".add-new").removeAttr("disabled");
-		}		
-    });
-	// Edit row on edit button click
-	$(document).on("click", ".edit", function(){		
-        $(this).parents("tr").find("td:not(:last-child)").each(function(){
-			$(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
-		});		
-		$(this).parents("tr").find(".add, .edit").toggle();
-		$(".add-new").attr("disabled", "disabled");
-    });
-	// Delete row on delete button click
-	$(document).on("click", ".delete", function(){
-        $(this).parents("tr").remove();
-		$(".add-new").removeAttr("disabled");
-    });
-});
-</script>
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 8px;}
+
+    </style>
 </head>
+
 <body>
-<div class="container-lg">
-    <div class="table-responsive">
-        <div class="table-wrapper">
-            <div class="table-title">
-                <div class="row">
-                    <div class="col-sm-8"><h2>User <b>Details</b></h2></div>
-                    <div class="col-sm-4">
-                        <button type="button" class="btn btn-info add-new"><i class="fa fa-plus"></i> Add New</button>
-                    </div>
-                </div>
-            </div>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>User ID</th>
-                        <th>Area</th>
-                        <th>City</th>
-                        <th>Postcode</th>
-                        <th>Contact Number</th>
-                        <th>User-type</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1000001</td>
-                        <td>Road no 6</td>
-                        <td>Dhaka</td>
-                        <td>1216</td>
-                        <td>171517211</td>
-                        <td>Financial Service Provider</td>
-                        <td>
-                            <a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-                            <a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                            <a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                            <button class="btn" style="width:100%"><i class="fa fa-download"></i> Download</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>1000002</td>
-                        <td>Road no 1</td>
-                        <td>Dhaka</td>
-                        <td>1205</td>
-                        <td>171517221</td>
-                        <td>Financial Service Provider</td>
-                        <td>
-                            <a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-                            <a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                            <a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                            <button class="btn" style="width:100%"><i class="fa fa-download"></i> Download</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>1000003</td>
-                        <td>Road no 9</td>
-                        <td>Dhaka</td>
-                        <td>1207</td>
-                        <td>171517227</td>
-                        <td>Financial Service Provider</td>
-                        <td>
-                            <a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-                            <a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                            <a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                            <button class="btn" style="width:100%"><i class="fa fa-download"></i> Download</button>
-                        </td>
-                    </tr>         
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>     
+
+<!-- Add button -->
+<br><br>
+<button onclick="location.href='add_user.php';">Add User</button>
+
 </body>
 </html>
+
