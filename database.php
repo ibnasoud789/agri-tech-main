c2d849ac58787d31b1ee4e97ce771249bd205c9d
ahnaftahmid19 <152405422+ahnaftahmid19@users.noreply.github.com>
2024-04-10 10:57:43 +0600

    investor

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+$hostName= "localhost";
+$dbUser="root";
+$dbPassword="";
+$dbName="practise2 (1)";
+$conn = new mysqli($hostName,$dbUser,$dbPassword,$dbName,3306);
+
+if ($conn -> connect_error){
+  die("Connection error:" .$conn->connect_error);
+}
+
+echo"";
