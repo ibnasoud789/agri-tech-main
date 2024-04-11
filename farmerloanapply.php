f596fcd451e92a4af6e48bf15da32e812e45c564
ibnasoud789 <ibnasoud0210@gmail.com>
2024-04-11 10:31:51 +0600

    Update

--- farmerloanapply.php
+++ farmerloanapply.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $userid = $_POST['user_id'];
     $loan_amount = $_POST['loan_amount'];
     $bank = $_POST['bank'];
-    $duration= $_POST['duration'];
+    $duration = $_POST['duration'];
 
     $sql = "INSERT INTO loan_application_t (farmer_name, farmer_id, loan_amount, preferred_bank, Verdict,duration) VALUES ('$user_name', '$userid', '$loan_amount', '$bank','Pending','$duration')";
     if ($conn->query($sql) === TRUE) {
@@ -20,15 +20,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 if (isset($_GET["id"])) {
-    $farmerID= $_GET["id"];
-    $query="SELECT Farmer_ID, CONCAT(fname,' ',mname,' ',lname) AS farmer_name FROM farmer_t WHERE Farmer_ID='$farmerID'";
-    $result = mysqli_query($conn,$query);
-    $row=mysqli_fetch_array($result);
-    $id= $row["Farmer_ID"];
-    $name= $row["farmer_name"];
-
-
-
+    $farmerID = $_GET["id"];
+    $query = "SELECT Farmer_ID, CONCAT(fname,' ',mname,' ',lname) AS farmer_name FROM farmer_t WHERE Farmer_ID='$farmerID'";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_array($result);
+    $id = $row["Farmer_ID"];
+    $name = $row["farmer_name"];
 }
 $conn->close();
 ?>
@@ -39,38 +36,56 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loan Application Form</title>
+    <style>
+        h2 {
+            text-align: center;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
 </head>
 
 <body>
     <h2>Loan Application Form</h2>
     <form action="" method="post">
-        <label for="user_name">Full Name:</label>
-        <input type="text" id="user_name" name="user_name" value="<?php echo $name; ?>" readonly><br><br>
+        <div>
+            <label for="user_name">Full Name:</label>
+            <input type="text" id="user_name" name="user_name" value="<?php echo $name; ?>" readonly><br><br>
+        </div>
+        <div>
+            <label for="user_id">Your ID:</label>
+            <input type="text" id="user_id" name="user_id" value="<?php echo $id; ?>" readonly><br><br>
+        </div>
+        <div>
+            <label for="loan_amount">Loan Amount:</label>
+            <input type="number" id="loan_amount" name="loan_amount" required><br><br>
+        </div>
+        <div>
+            <label for="duration">Loan Duration:</label>
+            <input type="text" id="duration" name="duration" required><br><br>
+        </div>
+        <div>
+            <label for="provider">Choose a Loan Provider:</label>
+            <select id="bank" name="bank" required>
+                <option value="">Select Bank</option>
+                <option value="Agrani Bank">Agrani Bank</option>
+                <option value="City Bank">City Bank</option>
+                <option value="Rajshahi Krishi Unnoyon Bank">Rajshahi Krishi Unnoyon Bank</option>
+                <option value="Bank Asia">Bank Asia</option>
+                <option value="Sonali Bank">Sonali Bank</option>
+                <option value="BDBL">BDBL</option>
+                <option value="Pubali Bank Limited">Pubali Bank Limited</option>
+                <option value="Islami Bank Limited">Islami Bank Limited</option>
+                <option value="Grameen Bank">Grameen Bank</option>
+                <option value="SBAC Bank">SBAC Bank</option>
+            </select><br><br>
 
-        <label for="user_id">Your ID:</label>
-        <input type="text" id="user_id" name="user_id" value="<?php echo $id; ?>" readonly><br><br>
-
-        <label for="loan_amount">Loan Amount:</label>
-        <input type="number" id="loan_amount" name="loan_amount" required><br><br>
-
-        <label for="duration" >Loan Duration:</label>
-        <input type="text" id="duration" name="duration" required><br><br>
-
-        <label for="provider">Choose a Loan Provider:</label>
-        <select id="bank" name="bank" required>
-            <option value="">Select Bank</option>
-            <option value="Agrani Bank">Agrani Bank</option>
-            <option value="City Bank">City Bank</option>
-            <option value="Rajshahi Krishi Unnoyon Bank">Rajshahi Krishi Unnoyon Bank</option>
-            <option value="Bank Asia">Bank Asia</option>
-            <option value="Sonali Bank">Sonali Bank</option>
-            <option value="BDBL">BDBL</option>
-            <option value="Pubali Bank Limited">Pubali Bank Limited</option>
-            <option value="Islami Bank Limited">Islami Bank Limited</option>
-            <option value="Grameen Bank">Grameen Bank</option>
-            <option value="SBAC Bank">SBAC Bank</option>
-        </select><br><br>
-
+        </div>
         <input type="submit" value="Submit">
     </form>
     <script>
@@ -88,7 +103,7 @@ $conn->close();
         window.onload = function() {
             <?php if (!empty($successMessage)) : ?>
                 alert("<?php echo $successMessage; ?>");
-                window.location.href = "farmer.php"; 
+                window.location.href = "farmer.php";
             <?php endif; ?>
         };
     </script>
--- farmerupdateportfolio.php
+++ farmerupdateportfolio.php
@@ -6,8 +6,8 @@ if (isset($_POST['update'])) {
   $contact = $_POST['contact'];
   $landsize = $_POST['landsize'];
   $area = $_POST['area'];
-  $city= $_POST['city'];
-  $postcode=$_POST['postcode'];
+  $city = $_POST['city'];
+  $postcode = $_POST['postcode'];
 
   $sql = "UPDATE `farmer_t` AS f JOIN `user` AS u ON f.Farmer_ID=u.userID  SET `contact_number`='$contact',`landsize`='$landsize',`area`='$area',`city`='$city',`postcode`='$postcode' WHERE Farmer_ID='$farmerid'";
 
@@ -51,32 +51,65 @@ if (isset($_GET['id'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Update Profile</title>
+  <style>
+    h2 {
+      text-align: center;
+    }
+
+    form {
+      gap: 10px;
+      width: 40%;
+    }
+    form div div{
+      display: flex;
+      flex-direction: column;
+      gap: 5px;
+    }
+    form div div input{
+      height: 20px;
+      width: 100%;
+      border-radius: 5px;
+      padding: 10px;
+      background-color: rgb(240, 247, 180);
+    }
+    button{
+      font-weight: bold;
+      width: 200px;
+      height: 45px;
+      margin-top: 10px;
+      border-radius: 10px;
+      color: white;
+      background-color: rgb(1, 62, 1);
+    }
+  </style>
 </head>
 
 <body>
   <form method="post">
     <h2>Update Personal Information</h2>
     <div>
-    <input type='hidden' name='id' value='<?php echo $farmerID; ?>'>
-      <label for="contact">Phone Number:</label>
-      <input type='number' name='contact' value='<?php echo $contact; ?>'>
-    </div>
-    <div>
-      <label for="landsize">Land Size:</label>
-      <input type='text' name='landsize' value='<?php echo $landsize; ?>'>
-    </div>
-    <div>
-      <label for="area">Area:</label>
-      <input type='text' name='area' value='<?php echo $area; ?>'>
-    </div>
-    <div>
-      <label for="city">City:</label>
-      <input type='text' name='city' value='<?php echo $city; ?>'>
-    </div>
-    <div>
-      <label for="postal">Postal Code:</label>
-      <input type='text' name='postcode' value='<?php echo $postcode; ?>'>
+      <div>
+        <input type='hidden' name='id' value='<?php echo $farmerID; ?>'>
+        <label for="contact">Phone Number:</label>
+        <input type='number' name='contact' value='<?php echo $contact; ?>'>
+      </div>
+      <div>
+        <label for="landsize">Land Size:</label>
+        <input type='text' name='landsize' value='<?php echo $landsize; ?>'>
+      </div>
+      <div>
+        <label for="area">Area:</label>
+        <input type='text' name='area' value='<?php echo $area; ?>'>
+      </div>
+      <div>
+        <label for="city">City:</label>
+        <input type='text' name='city' value='<?php echo $city; ?>'>
+      </div>
+      <div>
+        <label for="postal">Postal Code:</label>
+        <input type='text' name='postcode' value='<?php echo $postcode; ?>'>
+      </div>
     </div>
     <button type='submit' name='update'>Update</button>
   </form>
